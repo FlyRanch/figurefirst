4e881f452e550c39b133dd8d272e8928c8acaf65
Floris van Breugel <florisvb@gmail.com>
2016-06-10 16:27:18 -0700

    updated example to include multiple layers to hide template

--- examples/figure_templating_example.py
+++ examples/figure_templating_example.py
@@ -6,8 +6,7 @@ To create a templated figure, draw a rectangle, and add a figurefirst:figure tag
 
 layout = figurefirst.FigureLayout('figure_templating_example.svg' )
 layout.make_mplfigures()
-layout.append_figure_to_layer(layout.figures.values()[2], 'mpl_layer_2')
-layout.append_figure_to_layer(layout.figures.values()[3], 'mpl_layer_3')
-layout.append_figure_to_layer(layout.figures.values()[4], 'mpl_layer_4')
+layout.append_figure_to_layer(layout.figures['group2'], 'mpl_layer_2')
+layout.append_figure_to_layer(layout.figures['group3'], 'mpl_layer_3')
 layout.write_svg('figure_templating_example_output.svg')
 
