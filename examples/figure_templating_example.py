002bceaaabc944fb3230e8a50e7304790e08aa6c
Floris van Breugel <florisvb@gmail.com>
2016-06-10 15:59:12 -0700

    added basic instructions

--- examples/figure_templating_example.py
+++ examples/figure_templating_example.py
@@ -1,5 +1,9 @@
 import figurefirst
 
+'''
+To create a templated figure, draw a rectangle, and add a figurefirst:figure tag as if you were creating another figure. Then in addition to the figurefirst:name attribute, add a figurefirst:template attribute, whose value should correspond to the name of the figurefirst figure you want to use as the template. The template figure will be scaled to fill the box. 
+'''
+
 layout = figurefirst.FigureLayout('figure_templating_example.svg' )
 layout.make_mplfigures()
 layout.append_figure_to_layer(layout.figures.values()[2], 'mpl_layer_2')
