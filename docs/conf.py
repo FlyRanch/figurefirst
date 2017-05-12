ed18d62ee5047ec7c0377199252b6374a8630cd2
Floris van Breugel <florisvb@gmail.com>
2017-05-12 12:14:05 -0700

    hopefully enabled auto api stuff

--- docs/conf.py
+++ docs/conf.py
@@ -16,10 +16,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
@@ -59,7 +59,7 @@ author = u'Theodore Lindsay, Peter Weir, Floris van Breugel'
 # built documents.
 #
 # The short X.Y version.
-version = u''
+version = u'0.1'
 # The full version, including alpha/beta/rc tags.
 release = u'0.1'
 
@@ -87,7 +87,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -157,5 +157,5 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
+.. include:: ../README.md
 
