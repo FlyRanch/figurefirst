fc0e756dc34a9125819c3a5ab9cf82813e6818de
Chris Barnes <barnesc@janelia.hhmi.org>
2019-02-04 18:06:01 -0500

    use setuptools, list dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-from distutils.core import setup
-import setuptools
+from setuptools import setup
+
 setup(
     name='figurefirst',
     version='0.0.1',
     author='Floris van Breugel, Theodore Lindsay, Peter Weir',
     author_email='floris@caltech.edu',
-    packages = ['figurefirst'],
+    packages=['figurefirst'],
+    install_requires=["numpy", "matplotlib", "dill"],
     license='BSD',
     description='Matplotlib plotting stuff',
     long_description=open('README.md').read(),
 )
-
-
-
