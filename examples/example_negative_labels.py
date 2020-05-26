9abd9dfa6cc1571c2eb8429a0b70d912ecfba4d1
psilentp <thlindsay1@gmail.com>
2020-05-26 00:09:16 -0700

    test: add examples for encoding with negative numbers

---
+++ examples/example_negative_labels.py
@@ -0,0 +1,10 @@
+import figurefirst as fifi
+
+layout = fifi.svg_to_axes.FigureLayout('example_negative_labels.svg')
+layout.make_mplfigures()
+layout.fig.set_facecolor('None')
+ex = layout.axes['ex']
+ex.plot([1, 2], [3, 4])
+fifi.mpl_functions.adjust_spines(ex, spines='left', yticks=[-1, -2])
+layout.insert_figures('panels', cleartarget=True)
+layout.write_svg('negative_labels_output.svg')
